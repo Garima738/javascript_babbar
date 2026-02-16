13453685820df9b9d2d188774de4cbbb65c936e7
garima <garima@garimas-MacBook-Air.local>
2026-02-17 00:50:41 +0530

    changing css using js

---
+++ lec 49/lec.js
@@ -0,0 +1,23 @@
+//changing css using js 
+//sbse phele element ko uski id k thorugh fetch karege phir
+///Users/garima/Desktop/java s
+//script/lec 49/Screenshot 2026-02-17 at 12.00.39 AM.png isme steps h dekhne k liye
+
+
+//.css Text---->>> multiple inline style can be set by this in one time 
+//check screenshot-2
+
+//setAttribute()--->> is used to add a new attribute or change an existing attribute of an HTML element.
+//see screenesot-3;
+ 
+
+//see ss-4 something chgange if already class is preaent withe name divclass andagain we want to set value is change the divclass name to the value aur name given by ahain.
+
+//.className --->> refer ss-5
+ 
+
+//classList
+//add
+//remove
+//toggle
+//contain
